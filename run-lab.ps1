fa4c8e6c1898efaa42b96396244a8e95a2f2992e
Anthony Tran <ttran.usnavy@gmail.com>
2026-02-15 12:29:44 -0800

    chore: add Windows-friendly run wrappers

---
+++ run-lab.ps1
@@ -0,0 +1,9 @@
+param(
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [string[]]$AppArguments
+)
+
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$launcher = Join-Path $scriptDir 'Scripts/Run-OpenCodeLab.ps1'
+
+& $launcher @AppArguments
